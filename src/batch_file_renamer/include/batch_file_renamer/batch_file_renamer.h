d63a4e2e669dec212104d46dc0533d80eb799f74
Antonio Borondo <antonioborondo@outlook.com>
2019-04-08 11:00:21 +0100

    Create batch_file_renamer library

---
+++ src/batch_file_renamer/include/batch_file_renamer/batch_file_renamer.h
@@ -0,0 +1,11 @@
+#ifndef BATCH_FILE_RENAMER_H
+#define BATCH_FILE_RENAMER_H
+
+#include <vector>
+
+namespace batch_file_renamer
+{
+    bool rename_files(std::vector<std::string> files);
+}
+
+#endif
---
+++ src/batch_file_renamer/src/batch_file_renamer.cpp
@@ -0,0 +1,9 @@
+#include "batch_file_renamer/batch_file_renamer.h"
+
+namespace batch_file_renamer
+{
+    bool rename_files(std::vector<std::string> files)
+    {
+        return true;
+    }
+}
