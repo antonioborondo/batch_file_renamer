5a779e90fadcee70bdfd8b513f038aa2c6219f51
Antonio Borondo <antonioborondo@users.noreply.github.com>
2019-04-09 12:57:36 +0100

    Add WPF app (#5)

---
+++ src/wpf_app/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.DataContext = new MainWindowModel();
+        }
+
+        private void OpenDirectory_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                (this.DataContext as MainWindowModel).OpenDirectory();
+            }
+        }
+
+        private void RenameFiles_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                (this.DataContext as MainWindowModel).RenameFiles();
+            }
+        }
+    }
+}
---
+++ src/wpf_app/MainWindowModel.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace WpfApp
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string directory;
+
+        public string Directory
+        {
+            get
+            {
+                return directory;
+            }
+            set
+            {
+                directory = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Directory"));
+            }
+        }
+
+        private List<string> filenames;
+
+        public List<string> Filenames
+        {
+            get
+            {
+                return filenames;
+            }
+            set
+            {
+                filenames = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Filenames"));
+            }
+        }
+
+        public void OpenDirectory()
+        {
+            Filenames = new List<string> { "a.jpg", "b.jpg", "c.jpg", "d.jpg", "e.jpg" };
+        }
+
+        public void RenameFiles()
+        {
+            Filenames = new List<string> { "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg" };
+        }
+    }
+}
